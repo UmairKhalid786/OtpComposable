1ca82de4ffe44c17b663ff2389aa450bb44d4074
Umair Ali Khalid <umair.khalid786@outlook.com>
2023-12-16 20:51:02 +0100

    Code state added

--- app/src/main/java/com/techlads/otpcomposable/MainActivity.kt
+++ app/src/main/java/com/techlads/otpcomposable/MainActivity.kt
@@ -1,6 +1,9 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.techlads.otpcomposable
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedContent
@@ -18,27 +21,37 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.techlads.otpcomposable.ui.theme.OtpComposableTheme
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,17 +59,97 @@ class MainActivity : ComponentActivity() {
         setContent {
             OtpComposableTheme {
                 // A surface container using the 'background' color from the theme
+                val keyboard = LocalSoftwareKeyboardController.current
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = colorScheme.background
                 ) {
                     Column(
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier.fillMaxSize().padding(32.dp),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Text(text = "Enter OTP", style = MaterialTheme.typography.headlineSmall)
+                        val valid = remember { mutableStateOf(CodeState.Empty) }
+                        val coroutineScope = rememberCoroutineScope()
+
+                        Text(
+                            text = "Enter OTP",
+                            style = MaterialTheme.typography.headlineSmall,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
                         Spacer(modifier = Modifier.height(16.dp))
-                        CodeInput(4)
+                        CodeInput(4) {
+                            keyboard?.hide()
+                            coroutineScope.launch {
+                                valid.value = CodeState.Loading
+                                delay(2000)
+
+                                if (it == "1234") {
+                                    valid.value = CodeState.Valid
+                                } else {
+                                    valid.value = CodeState.Invalid
+                                }
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+                        AnimatedContent(targetState = valid.value) {
+                            when (it) {
+                                CodeState.Valid -> {
+                                    Text(
+                                        text = "A successful OTP submission",
+                                        style = MaterialTheme.typography.bodyMedium.copy(
+                                            color = Color.Green.compositeOver(colorScheme.secondary)
+                                        ),
+                                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                                    )
+                                }
+
+                                CodeState.Loading -> {
+                                    CircularProgressIndicator(
+                                        modifier = Modifier.size(32.dp), color = colorScheme.primary
+                                    )
+                                }
+
+                                CodeState.Invalid -> {
+                                    Row(verticalAlignment = Alignment.CenterVertically) {
+                                        Text(
+                                            text = "Unsuccessful OTP submission, please try again",
+                                            style = MaterialTheme.typography.bodyMedium.copy(
+                                                color = colorScheme.error.compositeOver(colorScheme.secondary)
+                                            )
+                                        )
+                                        TextButton(onClick = {
+                                            valid.value = CodeState.Empty
+                                        }) {
+                                            Text(
+                                                text = "Clear",
+                                                style = MaterialTheme.typography.bodyMedium
+                                            )
+                                        }
+                                    }
+                                }
+
+                                else -> {
+                                    Row(verticalAlignment = Alignment.CenterVertically) {
+                                        Text(
+                                            text = "Didn't got the OTP? Resend",
+                                            style = MaterialTheme.typography.bodyMedium,
+                                        )
+                                        TextButton(onClick = {
+                                            Toast.makeText(
+                                                this@MainActivity,
+                                                "OTP request sent, you can resend again one one minute",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }) {
+                                            Text(
+                                                text = "Resend",
+                                                style = MaterialTheme.typography.bodyMedium
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -64,13 +157,23 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+enum class CodeState {
+    Empty, Valid, Invalid, Loading
+}
+
 @Composable
-fun CodeInput(codeLength: Int) {
+fun CodeInput(codeLength: Int, onCodeEnter: (String) -> Unit = {}) {
     val code = remember { mutableStateOf("") }
 
     val focusRequester = FocusRequester()
     LaunchedEffect(Unit) {
         focusRequester.requestFocus()
+    }
+
+    LaunchedEffect(code.value) {
+        if (code.value.length == codeLength) {
+            onCodeEnter(code.value)
+        }
     }
 
     Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
@@ -123,13 +226,11 @@ private fun CodeChar(text: String, focused: Boolean = false) {
                 border = BorderStroke(
                     2.dp,
                     color = colorScheme.primary.copy(alpha = if (focused) animatedOpacity else 0f)
-                ),
-                shape = shapes.small
+                ), shape = shapes.small
             )
             .width(56.dp)
             .aspectRatio(1f)
-            .fillMaxWidth(),
-        contentAlignment = Alignment.Center
+            .fillMaxWidth(), contentAlignment = Alignment.Center
     ) {
         AnimatedContent(targetState = text, label = "") {
             Text(
